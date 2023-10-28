9f9aa2aa90a351a8b9f1f129e9a63cefede5697e
Yarden Asado <yarden.asado@seekingalpha.com>
2023-10-28 14:04:22 -0400

    use a custom source for nodejs

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,11 +1,13 @@
 # Packages that are neccesary for Rocket.Chat installation and configuration
 class rocketchat::packages(
   $nodejs_version,
+  $nodejs_source,
   $nodejs_deps
 ) {
 
   class { 'nodejs':
     version    => $nodejs_version,
+    source     => $nodejs_source,
     build_deps => $nodejs_deps,
     before     => Exec['npm install']
   }
